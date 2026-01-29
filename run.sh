634b9c6cb778a056b96dfc43635a779c3e8daa5d
Raúl Adamuz Cano <radamuzc@gmail.com>
2026-01-29 16:42:10 +0100

    feat: add instance profile name for EC2 instance creation

--- run.sh
+++ run.sh
@@ -223,6 +223,7 @@ echo -e "${GREEN}Fin Bloque Elegir el instance type de AWS${NC}"
 # Crear policy, role e instance profile
 echo -e "${CYAN}Inicio Bloque Crear policy, role e instance profile${NC}"
 bash scripts/create-policy-role-and-instance-profile.sh "$AWS_PROFILE" "$AWS_REGION" "$APP_NAME"
+INSTANCE_PROFILE_NAME="$APP_NAME-instance-profile"
 echo -e "${GREEN}Fin Bloque Crear policy, role e instance profile${NC}"
 # Fin Crear policy, role e instance profile
 
@@ -268,6 +269,7 @@ if $CREATE_EC2; then
       --key-name "$PEM_KEY_NAME" \
       --security-group-ids "$SECURITY_GROUP_ID" \
       --subnet-id "$SUBNET_ID" \
+      --iam-instance-profile Name="$INSTANCE_PROFILE_NAME" \
       --user-data file://"$USER_DATA_PATH" \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$APP_NAME}]" | jq)
     # Fin Arrancar nueva instancia EC2
